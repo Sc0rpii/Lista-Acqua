e4fe22487fb53903c6a7182f6e24ef14487f7b31
Sc0rpii <mirkofreschi66@gmail.com>
2023-12-04 00:45:08 +0100

    Try to modifile json file and go print it

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var count = 0;
+const url = './db/lista.json';
+const buttonSubmit = document.querySelector("#submit");
 
 function aggiungiRiga(nStanza, nomeCognome, nCasse) {
     var table = document.getElementById("tabella").getElementsByTagName('tbody')[0];
@@ -21,10 +23,55 @@ function somWater(acqua) {
     return count += parseInt(acqua);
 }
 
-function update() {
-    // Fetch file json
-    const url = './db/lista.json';
+// Inserisci dati nell'array json
+buttonSubmit.addEventListener("click", function(){
+    var name = document.querySelector("#name").value;
+    var room = document.querySelector("#Number_room").value;
+    var water = document.querySelector("#Number_water").value;
 
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Trova l'ID massimo attuale
+            const maxId = data.length > 0 ? Math.max(...data.map(item => item.id)) : -1;
+
+            // Crea un nuovo oggetto con un ID incrementato automaticamente
+            const nuovoElemento = {
+                id: maxId + 1,
+                name: name,
+                room: room,
+                water: water
+            };
+
+            // Aggiungi il nuovo oggetto all'array
+            data.push(nuovoElemento);
+
+            // Salva l'array aggiornato nel tuo file JSON
+            const jsonString = JSON.stringify(data, null, 2);
+
+            // Effettua una richiesta per scrivere il file JSON sul server
+            fetch(url, {
+                method: 'PUT', // o 'POST' a seconda della tua API
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: jsonString
+            })
+                .then(response => response.json())
+                .then(updatedData => {
+                    // Logga eventuali dati aggiornati
+                    console.log(updatedData);
+                })
+                .catch(error => console.error('Errore nella scrittura del file JSON:', error));
+        })
+        .catch(error => console.error('Errore nel caricamento del file JSON:', error));
+    aggiungiRiga(room, name, water);
+});
+
+//Aggiorna la tabella
+function update() {
+
+    // Fetch file json
     fetch(url)
         .then(response => {
             // Controlla se la risposta è positiva (status 200)
@@ -42,64 +89,6 @@ function update() {
             });
         })
         .catch(error => {
-            console.error('Si è verificato un errore:', error);
-        });
+        console.error('Si è verificato un errore:', error);
+    });
 }
-
-// Inserisci dati nell'array json
-document.querySelector("#submit").addEventListener("click", function () {
-    var name = document.querySelector("#name").value;
-    var room = document.querySelector("#Number_room").value;
-    var water = document.querySelector("#Number_water").value;
-
-    fetch('./db/lista.json')
-        .then(response => response.json())
-        .then(data => {
-            // Se l'array è vuoto, inizializzalo con un nuovo elemento
-            if (data.length === 0) {
-                data = [{
-                    id: 0,
-                    stanza: room,
-                    name: name,
-                    nCasse: water
-                }];
-            } else {
-                // Trova l'ID massimo attuale
-                const maxId = Math.max(...data.map(item => item.id));
-
-                // Aggiungi un nuovo elemento con un ID automatico
-                const nuovoElemento = {
-                    id: maxId + 1,
-                    stanza: room,
-                    name: name,
-                    nCasse: water
-                };
-
-                // Aggiungi il nuovo elemento all'array
-                data.push(nuovoElemento);
-            }
-
-            // Salva l'array aggiornato nel tuo file JSON
-            const jsonString = JSON.stringify(data, null, 2);
-
-            // Effettua una richiesta per scrivere il file JSON sul server
-            fetch('./db/lista.json', {
-                method: 'POST', // o 'PUT' a seconda della tua API
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: jsonString
-            })
-                .then(response => response.json())
-                .then(updatedData => {
-                    // Logga eventuali dati aggiornati
-                    console.log(updatedData);
-
-                    // Esegui l'aggiornamento nell'interfaccia dopo che il file è stato modificato
-                    update();
-                })
-                .catch(error => console.error('Errore nella scrittura del file JSON:', error));
-        })
-        .catch(error => console.error('Errore nel caricamento del file JSON:', error));
-        update();
-});
